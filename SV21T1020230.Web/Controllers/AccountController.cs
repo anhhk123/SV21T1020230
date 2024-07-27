d267c702697d98ca7cccb64f0b7848033021fa67
anhhk123 <anhcao244@gmail.com>
2024-07-27 09:16:52 +0700

    commit-27-7

---
+++ SV21T1020230.Web/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SV21T1020230.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+    }
+}
